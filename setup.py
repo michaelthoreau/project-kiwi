e8b6ddd6d21957e054ec4f9091b9a032a1da767b
Michael Thoreau <michael@project-kiwi.org>
2022-04-13 22:37:11 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
   name = 'projectkiwi',         # How you named your package folder (MyLib)
   packages = ['projectkiwi'],   # Chose the same as "name"
-  version = '0.1.2',      # Start with a small number and increase it with every change you make
+  version = '0.2.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python tools for project-kiwi.org',   # Give a short description about your library
   long_description=long_description,
@@ -16,7 +16,7 @@ setup(
   url = 'https://github.com/michaelthoreau/projectkiwi',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/michaelthoreau/projectkiwi/archive/refs/tags/v0.1.2.tar.gz',    # I explain this later on
   keywords = ['GIS', 'ML', 'OTHERBUZZWORDS'],   # Keywords that define your package best
-  install_requires=['requests'],
+  install_requires=['requests', 'numpy', 'PIL'],
   python_requires='>=3.3',
   classifiers=[
         "Programming Language :: Python :: 3",
