ed0bc3c96c500a03e3c7e10aa877ea4b97fc4b0f
Michael Thoreau <michaelthoreau@Michaels-MacBook-Pro.local>
2022-11-28 17:51:43 -0800

    added extra params to tiles in tileList

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-VERSION = '0.5.5'
+VERSION = '0.5.7'
 
 setup(
   name = 'projectkiwi',
