c9e45eaf4494f791908694ace739f3552f35209c
Michael Thoreau <michaelthoreau@Michaels-MacBook-Pro.local>
2023-02-01 16:52:16 -0800

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-VERSION = '0.5.9'
+VERSION = '0.6.0'
 
 setup(
   name = 'projectkiwi',
